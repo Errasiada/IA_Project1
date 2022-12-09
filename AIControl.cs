8fb87c15aca2b04c0d1920f61148a969218a1e39
Noa Dionis <114673717+Errasiada@users.noreply.github.com>
2022-12-09 11:38:18 +0100

    Add files via upload

---
+++ AIControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform objective;
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.destination = objective.position;
+    }
+
+    
+}
